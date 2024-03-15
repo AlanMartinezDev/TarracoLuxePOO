2f4622bbfa949f4e76be7bc185e54b202a3aa171
AlanMartinezDev <alanmartiola@gmail.com>
2024-03-15 10:26:21 +0100

    footer y cambio de año automático

--- index.php
+++ index.php
@@ -202,20 +202,6 @@ include './includes/templates/header.php';
     </div>
   </section>
 </div>
-<footer class="footer seccion">
-  <div class="contenedor contenedor-footer">
-    <nav class="navegacion">
-      <a href="nosotros.html">Nosotros</a>
-      <a href="anuncios.html">Anuncios</a>
-      <a href="blog.html">Blog</a>
-      <a href="contacto.html">Contacto</a>
-    </nav>
-  </div>
-  <p class="copyright">
-    Todos los Derechos Reservados 2024 &copy; TarracoLuxe
-  </p>
-</footer>
-<script src="build/js/bundle.min.js"></script>
-</body>
-
-</html>
+<?php
+include './includes/templates/footer.php';
+?>
