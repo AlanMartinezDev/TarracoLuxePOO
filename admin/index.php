6b40eaa507a65288ce80ce77fe13f182c948bbcc
AlanMartinezDev <alanmartiola@gmail.com>
2024-03-18 16:53:34 +0100

    listado de propiedades por PHP

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,18 @@
 <?php
+// Importar la conexión
+require '../includes/config/database.php';
+$db = conectarDB();
+
+// Escribir el query
+$query = "SELECT * FROM propiedades";
+
+// Consultar la base de datos
+$resultadoConsulta = mysqli_query($db, $query);
+
+// Muestra mensaje condicional
 $resultado = $_GET['resultado'] ?? null;
 
+// Incluye un template
 require '../includes/funciones.php';
 incluirTemplate('header');
 ?>
@@ -10,7 +22,35 @@ incluirTemplate('header');
         <p class="alerta exito">Propiedad creada correctamente</p>
     <?php endif; ?>
     <a href="/TarracoLuxe/admin/propiedades/crear.php" class="boton boton-verde">Nueva propiedad</a>
+    <table class="propiedades">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Título</th>
+                <th>Imagen</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php while ($propiedad = mysqli_fetch_assoc($resultadoConsulta)) : ?>
+                <tr>
+                    <td><?php echo $propiedad['id']; ?></td>
+                    <td><?php echo $propiedad['titulo']; ?></td>
+                    <td><img src="/TarracoLuxe/imagenes/<?php echo $propiedad['imagen']; ?>" alt="Imagen de la casa en la playa" class="imagen-tabla"></td>
+                    <td><?php echo $propiedad['precio']; ?> €</td>
+                    <td>
+                        <a href="propiedades/actualizar.php?id=<?php echo $propiedad['id']; ?>" class="boton-amarillo-block">Actualizar</a>
+                        <a href="propiedades/borrar.php?id=<?php echo $propiedad['id']; ?>" class="boton-rojo-block">Borrar</a>
+                    </td>
+                </tr>
+            <?php endwhile; ?>
+        </tbody>
+    </table>
 </main>
 <?php
+// Cerrar la conexión (opcional)
+mysqli_close($db);
+
 incluirTemplate('footer');
 ?>
