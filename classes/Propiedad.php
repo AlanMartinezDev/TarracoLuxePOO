33b0eb22756b8ef837e73e1b6a7791434cf1e866
AlanMartinezDev <alanmartiola@gmail.com>
2024-04-10 16:58:01 +0200

    Add attributes

--- classes/Propiedad.php
+++ classes/Propiedad.php
@@ -1,6 +1,31 @@
 <?php
+
 namespace App;
 
-class Propiedad {
-    
+class Propiedad
+{
+    public $id;
+    public $titulo;
+    public $precio;
+    public $imagen;
+    public $descripcion;
+    public $habitaciones;
+    public $wc;
+    public $plazas;
+    public $creado;
+    public $vendedorId;
+
+    public function __construct($args = [])
+    {
+        $this->id = $args["id"] ?? "";
+        $this->titulo = $args["titulo"] ?? "";
+        $this->precio = $args["precio"] ?? "";
+        $this->imagen = $args["imagen"] ?? "";
+        $this->descripcion = $args["descripcion"] ?? "";
+        $this->habitaciones = $args["habitaciones"] ?? "";
+        $this->wc = $args["wc"] ?? "";
+        $this->plazas = $args["plazas"] ?? "";
+        $this->creado = $args["creado"] ?? "";
+        $this->vendedorId = $args["vendedorId"] ?? "";
+    }
 }
