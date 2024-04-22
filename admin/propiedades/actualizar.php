bc42947551159a393e8b1e95797af545bca03924
AlanMartinezDev <alanmartiola@gmail.com>
2024-04-22 16:24:24 +0200

    Update with Product model

--- admin/propiedades/actualizar.php
+++ admin/propiedades/actualizar.php
@@ -1,6 +1,12 @@
 <?php
-require '../../includes/funciones.php';
+
+use App\Propiedad;
+
+require '../../includes/app.php';
+
 estaAutenticado();
+
+$db = conectarDB();
 
 // Validar por ID válido
 $id = $_GET['id'];
@@ -10,13 +16,8 @@ if (!$id) {
     header('Location: /TarracoLuxe/admin');
 }
 
-require '../../includes/config/database.php';
-$db = conectarDB();
-
 // Obtener los datos de la propiedad
-$consulta = "SELECT * FROM propiedades WHERE id = {$id}";
-$resultado = mysqli_query($db, $consulta);
-$propiedad = mysqli_fetch_assoc($resultado);
+$propiedad = Propiedad::find($id);
 
 // Consultar para obtener los vendedores
 $consulta = "SELECT * FROM vendedores";
@@ -24,15 +25,6 @@ $resultado = mysqli_query($db, $consulta);
 
 // Array con mensajes de errores
 $errores = [];
-
-$titulo = $propiedad['titulo'];
-$precio = $propiedad['precio'];
-$descripcion = $propiedad['descripcion'];
-$habitaciones = $propiedad['habitaciones'];
-$wc = $propiedad['wc'];
-$plazas = $propiedad['plazas'];
-$vendedorId = $propiedad['vendedorId'];
-$imagenPropiedad = $propiedad['imagen'];
 
 // Ejecutar el código después de que el usuario envía el formulario
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -137,36 +129,7 @@ incluirTemplate('header');
         </div>
     <?php endforeach ?>
     <form method="post" class="formulario" enctype="multipart/form-data">
-        <fieldset>
-            <legend>Información general</legend>
-            <label for="titulo">Título:</label>
-            <input type="text" id="titulo" name="titulo" value="<?php echo $titulo; ?>" placeholder="Título de la propiedad">
-            <label for="precio">Precio:</label>
-            <input type="number" id="precio" name="precio" value="<?php echo $precio; ?>" placeholder="Precio de la propiedad">
-            <label for="imagen">Imagen:</label>
-            <input type="file" id="imagen" name="imagen" value="<?php echo $imagen; ?>" accept="image/jpeg, image/png">
-            <img src="/TarracoLuxe/imagenes/<?php echo $imagenPropiedad; ?>" alt="" class="imagen-small">
-            <label for="descripcion">Descripción</label>
-            <textarea id="descripcion" name="descripcion"><?php echo $descripcion; ?></textarea>
-        </fieldset>
-        <fieldset>
-            <legend>Información de la propiedad</legend>
-            <label for="habitaciones">Habitaciones:</label>
-            <input type="number" id="habitaciones" name="habitaciones" value="<?php echo $habitaciones; ?>" placeholder="Número de habitaciones" min="1" max="9">
-            <label for="wc">Baños:</label>
-            <input type="number" id="wc" name="wc" value="<?php echo $wc; ?>" placeholder="Número de baños" min="1" max="9">
-            <label for="plazas">Plazas:</label>
-            <input type="number" id="plazas" name="plazas" value="<?php echo $plazas; ?>" placeholder="Número de plazas" min="1" max="9">
-        </fieldset>
-        <fieldset>
-            <legend>Vendedor</legend>
-            <select name="vendedor">
-                <option disabled selected>-- Selecciona un vendedor --</option>
-                <?php while ($vendedor = mysqli_fetch_assoc($resultado)) : ?>
-                    <option <?php echo $vendedorId === $vendedor['id'] ? 'selected' : ''; ?> value="<?php echo $vendedor['id']; ?>"><?php echo $vendedor['nombre'] . " " . $vendedor['apellido']; ?></option>
-                <?php endwhile ?>
-            </select>
-        </fieldset>
+        <?php include "../../includes/templates/formulario_propiedades.php" ?>
         <input type="submit" value="Actualizar propiedad" class="boton boton-verde">
     </form>
     <a href="/TarracoLuxe/admin" class="boton boton-verde">Volver</a>
