d8556a74927e238631d7e6989ae6f06455c66187
AlanMartinezDev <alanmartiola@gmail.com>
2024-04-23 14:50:49 +0200

    Change name value

--- includes/templates/formulario_propiedades.php
+++ includes/templates/formulario_propiedades.php
@@ -1,25 +1,25 @@
 <fieldset>
     <legend>Información general</legend>
     <label for="titulo">Título:</label>
-    <input type="text" id="titulo" name="titulo" value="<?php echo s($propiedad->titulo); ?>" placeholder="Título de la propiedad">
+    <input type="text" id="titulo" name="propiedad[titulo]" value="<?php echo s($propiedad->titulo); ?>" placeholder="Título de la propiedad">
     <label for="precio">Precio:</label>
-    <input type="number" id="precio" name="precio" value="<?php echo s($propiedad->precio); ?>" placeholder="Precio de la propiedad">
+    <input type="number" id="precio" name="propiedad[precio]" value="<?php echo s($propiedad->precio); ?>" placeholder="Precio de la propiedad">
     <label for="imagen">Imagen:</label>
-    <input type="file" id="imagen" name="imagen" value="<?php echo $propiedad->imagen; ?>" accept="image/jpeg, image/png">
+    <input type="file" id="imagen" name="propiedad[imagen]" value="<?php echo $propiedad->imagen; ?>" accept="image/jpeg, image/png">
     <?php if ($propiedad->imagen) { ?>
         <img src="/TarracoLuxe/imagenes/<?php echo $propiedad->imagen ?>" class="imagen-small">
     <?php } ?>
     <label for="descripcion">Descripción</label>
-    <textarea id="descripcion" name="descripcion"><?php echo s($propiedad->descripcion); ?></textarea>
+    <textarea id="descripcion" name="propiedad[descripcion]"><?php echo s($propiedad->descripcion); ?></textarea>
 </fieldset>
 <fieldset>
     <legend>Información de la propiedad</legend>
     <label for="habitaciones">Habitaciones:</label>
-    <input type="number" id="habitaciones" name="habitaciones" value="<?php echo s($propiedad->habitaciones); ?>" placeholder="Número de habitaciones" min="1" max="9">
+    <input type="number" id="habitaciones" name="propiedad[habitaciones]" value="<?php echo s($propiedad->habitaciones); ?>" placeholder="Número de habitaciones" min="1" max="9">
     <label for="wc">Baños:</label>
-    <input type="number" id="wc" name="wc" value="<?php echo s($propiedad->wc); ?>" placeholder="Número de baños" min="1" max="9">
+    <input type="number" id="wc" name="propiedad[wc]" value="<?php echo s($propiedad->wc); ?>" placeholder="Número de baños" min="1" max="9">
     <label for="plazas">Plazas:</label>
-    <input type="number" id="plazas" name="plazas" value="<?php echo s($propiedad->plazas); ?>" placeholder="Número de plazas" min="1" max="9">
+    <input type="number" id="plazas" name="propiedad[plazas]" value="<?php echo s($propiedad->plazas); ?>" placeholder="Número de plazas" min="1" max="9">
 </fieldset>
 <fieldset>
     <legend>Vendedor</legend>
