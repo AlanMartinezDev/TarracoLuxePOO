dedef4da05fd4f817b3ec4e4544d9ab6d1eacde1
AlanMartinezDev <alanmartiola@gmail.com>
2024-04-23 16:59:51 +0200

    Update DB

--- tarracoluxe_db.sql
+++ tarracoluxe_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-03-2024 a las 16:18:43
+-- Tiempo de generación: 23-04-2024 a las 16:59:21
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -40,6 +40,34 @@ CREATE TABLE `propiedades` (
   `vendedorId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `propiedades`
+--
+
+INSERT INTO `propiedades` (`id`, `titulo`, `precio`, `imagen`, `descripcion`, `habitaciones`, `wc`, `plazas`, `creado`, `vendedorId`) VALUES
+(26, ' Casa en la montaña (update)', 150000.00, '5ed9eaef074954c5df5c6a4955b5ee08.jpg', 'Casa en la montañaCasa en la montañaCasa en la montañaCasa en la montañaCasa en la montaña', 3, 3, 3, '2024-04-23', 1),
+(27, ' Casa en la playa update', 1500000.00, '27da6f8f58f366ebba19778eb2007dd5.jpg', 'Casa en la playaCasa en la playaCasa en la playaCasa en la playaCasa en la playaCasa en la playaCasa en la playaCasa en la playaCasa en la playa', 3, 4, 5, '2024-04-23', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(8) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` char(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `email`, `password`) VALUES
+(1, 'alan@tarracoluxe.com', '$2y$10$f6/e78e4Pi1R2sLLrWMw2.knmsswOR4TkDDTqDyYEPJmmWdWrQbeC'),
+(2, 'correo@correo.com', '$2y$10$wJ.GKlZ.An6TQBiZSo/Y1.ZHFCg/kjj39W27aU39FVcKVLojo2NYC');
+
 -- --------------------------------------------------------
 
 --
@@ -73,6 +101,12 @@ ALTER TABLE `propiedades`
   ADD KEY `vendedores_id` (`vendedorId`);
 
 --
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `vendedores`
 --
 ALTER TABLE `vendedores`
@@ -86,7 +120,13 @@ ALTER TABLE `vendedores`
 -- AUTO_INCREMENT de la tabla `propiedades`
 --
 ALTER TABLE `propiedades`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `vendedores`
